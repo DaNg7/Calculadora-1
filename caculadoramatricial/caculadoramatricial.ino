a1a81878053d32be508d0a747881782edca0e524
danielg7 <danielgrecia7@gmail.com>
2018-03-05 15:27:25 -0500

    modificacion de caculadoramatricial

--- caculadoramatricial/caculadoramatricial.ino
+++ caculadoramatricial/caculadoramatricial.ino
@@ -1,19 +1,41 @@
-int numero1=3;
-int columna1=7;
-int  leido;
+int pinf[4]={2,3,4,5};
+int pinc[4]={9,8,7,6,};
+int  leido=0;
 
-void setup() {
-Serial.begin(9600);
-pinMode(numero1,INPUT);
-pinMode(columna1,OUTPUT);
+  void setup() {
+  Serial.begin(9600);
+  for(int i=0;i<3;i++){
+    pinMode(pinc[i],OUTPUT);
+    pinMode(pinf[i],INPUT_PULLUP);
+ }
 }
 
-void loop() {
- numero1 = HIGH;
- leido=digitalRead(columna1);
-if (leido==HIGH){
+  void loop() {
+  for(int a=0;a<3;a++){
+    digitalWrite(pinc[a], LOW);
+    for(int k=0;k<3;k++){
+      leido=digitalRead(pinf[k]);
+      if (leido==LOW){
+         dat(pinc[a],pinf[k]);
 
- Serial.print("hay  un numero");
+        }
+      }
+    }
+  }  
+
+
+
+
+
+
+  int dat (int columna , int fila ){
+
+   if (columna==8 && fila==4)Serial.print(1);
+   if (columna==8 && fila==3)Serial.print(4);
+   if (columna==8 && fila==2)Serial.print(7);
+    
+
+
+    }
+
   
-}
-}
