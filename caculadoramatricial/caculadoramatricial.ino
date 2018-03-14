9e94c70c1ec0c6a6d3fe92aaf40f29d0ae5b298e
danielg7 <danielgrecia7@gmail.com>
2018-03-14 13:14:20 -0500

    ultima modificacion

--- caculadoramatricial/caculadoramatricial.ino
+++ caculadoramatricial/caculadoramatricial.ino
@@ -3,7 +3,10 @@ int pinc[4]={9,8,7,6,};
 int  leido=0;
 int  leido2=0;
 char d;
-  void setup() {
+
+char teclaPrecionada=0;
+
+void setup() {
   Serial.begin(9600);
   for(int i=0;i<4;i++){
     pinMode(pinc[i],OUTPUT);
@@ -13,39 +16,45 @@ char d;
  
 }
 
-  void loop() {
+void loop() {
+  teclaPrecionada = 0;
   for(int a=0;a<4;a++){
     digitalWrite(pinc[a], LOW);
+        
     for(int k=0;k<4;k++){
       leido=digitalRead(pinf[k]);
     
       if (leido==LOW ){
+          teclaPrecionada=1;
           char x=dat(pinc[a], pinf[k]);
            
            if (d!=x){
             Serial.print(x); 
-            d=x;     
-              
-       }
-            
-        }
-      }
-      digitalWrite(pinc[a], HIGH);
+            d=x;         
+           }
+           delay(100);       
+       }        
     }
-  }  
-
-
-
-
-
-
-  char dat (int columna , int fila ){
-   
-   
-   if (columna==8 && fila==5) return '2';
     
-   
-   
+    digitalWrite(pinc[a], HIGH);
+    
+  }
+  if(!teclaPrecionada){
+    d="";
+  //    Serial.print("reiniciando");
+  }
+  
+}  
+
+
+
+
+
+
+char dat (int columna , int fila ){
+      
+   if (columna==8 && fila==5) return '2';
+      
    if (columna==8 && fila==2)return  '0';
    if (columna==8 && fila==4)return '5';
    if (columna==8 && fila==3)return '8';
@@ -55,18 +64,18 @@ char d;
    if (columna==9 && fila==4)return '4';
    if (columna==9 && fila==3)return '7';
 
-    if (columna==7 && fila==5)return '3';
+   if (columna==7 && fila==5)return '3';
    if (columna==7 && fila==2)return '#';
    if (columna==7 && fila==4)return '6';
    if (columna==7 && fila==3)return '9';
 
-   if (columna==6 && fila==5)return 'A';;
-   if (columna==6 && fila==2)return 'D';;
+   if (columna==6 && fila==5)return 'A';
+   if (columna==6 && fila==2)return 'D';
    if (columna==6 && fila==4)return 'B';
    if (columna==6 && fila==3)return 'C';
+   
+   return "";
   
-  
-
-    }
+}
 
   
